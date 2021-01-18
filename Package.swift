349880277bc75ea0c94b1668f450d85d8ce0ceff
William Archimede <william.archimede@protonmail.com>
2021-01-18 23:10:47 +0100

    Add ArgumentParser dependency

--- Package.swift
+++ Package.swift
@@ -5,18 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "cpd2xcode",
+    products: [
+        .executable(name: "cpd2xcode", targets: ["cpd2xcode"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "cpd2xcode",
-            dependencies: []),
+        .target(name: "cpd2xcode", dependencies: [.product(name:"ArgumentParser", package: "swift-argument-parser")]),
         .testTarget(
-            name: "cpd2xcodeTests",
-            dependencies: ["cpd2xcode"]),
+            name: "cpd2xcodeTests", dependencies: ["cpd2xcode"])
     ]
 )
